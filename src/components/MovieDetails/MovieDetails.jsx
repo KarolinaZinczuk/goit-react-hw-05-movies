2546065ca7c9064d0d30f98e7961b0ba5cc44e11
KarolinaZinczuk <104589501+KarolinaZinczuk@users.noreply.github.com>
2023-02-12 21:26:21 +0100

    Create MovieDetails.jsx

---
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -0,0 +1,65 @@
+import { useParams, Link, Outlet } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getDetails, IMAGE_URL } from "API/fetchMovies";
+
+const MovieDetails = () => {
+    const [movie, setMovie] = useState(null);
+    const [error, setError] = useState('');
+    const { movieId } = useParams();
+
+    useEffect(() => {
+        getDetails(movieId)
+            .then(results => setMovie(results))
+            .catch(error => setError(error));
+    }, [movieId]);
+    
+    return (
+        <>
+            {!movie ? (
+                <p> This movie is not found</p>
+            ) : (
+                <>
+                    <Link to="/">
+                        <button type="button">GO BACK</button>
+                    </Link>
+                    <div>
+                        <div>
+                            <img
+                                src={movie.poster_path
+                                    ? IMAGE_URL + movie.poster_path
+                                    : `https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/300px-No_image_available.svg.png`
+                                }
+                                alt={movie.title}
+                                width=""
+                                height=""
+                            />
+                        </div>
+                    
+                        <div>
+                            <h2>{movie.title}</h2>
+                            <p>User score: {`${movie.vote_average * 10}`}%</p>
+                            <h3>Overview</h3>
+                            <p>{`${movie.overview}`}</p>
+                            <h3>Genres</h3>
+                            <p>{`${movie.genres.map(genre => genre.name).join(', ')}`}</p>
+                        </div>
+                    </div>
+                </>
+            )}
+
+            <h2>Additional information</h2>
+            <div>
+                <Link to={"cast"}>
+                    <button type="button">Cast</button>
+                </Link>
+
+                <Link to={"reviewes"}>
+                    <button type="button">Reviews</button>
+                </Link>
+            </div>
+            <Outlet/>
+        </>
+    );
+};
+
+export default MovieDetails;
