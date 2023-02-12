7fa957a6453985af63ac181a3c98e2c770a7b577
KarolinaZinczuk <104589501+KarolinaZinczuk@users.noreply.github.com>
2023-02-12 21:26:27 +0100

    Create Nav.jsx

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,15 @@
+import { NavLink } from "react-router-dom";
+
+const Nav = () => {
+    <nav>
+        <NavLink to="/">
+            Home
+        </NavLink>
+        
+        <NavLink to="movies">
+            Movies
+        </NavLink>
+    </nav>
+};
+
+export default Nav;
