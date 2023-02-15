42f55c803e1def32e149266f8a0cad77a3220fce
KarolinaZinczuk <104589501+KarolinaZinczuk@users.noreply.github.com>
2023-02-15 10:44:33 +0100

    Update Cast.jsx

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getCast, IMAGE_URL } from "API/fetchMovies";
 
+import styles from './Cast.module.css';
+
 const Cast = () => {
     const [cast, setCast] = useState([]);
     const { movieId } = useParams();
@@ -13,26 +15,29 @@ const Cast = () => {
 
     return (
         <>
-            {cast.length === 0 ? ("There is no cast ..." ): (
-                <ul>
-                    {cast.map(
-                        ({ id, name, profile_path, original_name, character }) => (
-                            <li key={id}>
-                                <img src={profile_path
-                                    ? `${IMAGE_URL}${profile_path}`
-                                    : `https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/300px-No_image_available.svg.png`
-                                }
-                                    alt={original_name} />
-                                <h3>{name}</h3>
-                                <p> Charakter: {character}</p>
-                            </li>
-                        )
-                    )}
-                </ul>
-            )};
+            <div className={styles.list_section}>
+                {cast.length === 0 ? ("There is no cast ...") : (
+                    <ul className={styles.list}>
+                        {cast.map(
+                            ({ id, name, profile_path, original_name, character }) => (
+                                <li key={id} className={styles.item}>
+                                    <img src={profile_path
+                                        ? `${IMAGE_URL}${profile_path}`
+                                        : `https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/300px-No_image_available.svg.png`
+                                    }
+                                        alt={original_name}
+                                        className={styles.item_image} />
+                                    <h3>{name}</h3>
+                                    <p className={styles.item_name}> Charakter: {character}</p>
+                                </li>
+                            )
+                        )}
+                    </ul>
+                )};
+            </div>
         </>
-    )
-}
+    );
+};
 
 
 
