ca9ee1cbc9688644ac9ba856c5045e79f3302a60
KarolinaZinczuk <104589501+KarolinaZinczuk@users.noreply.github.com>
2023-02-15 10:44:12 +0100

    Update Nav.jsx

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,15 +1,27 @@
 import { NavLink } from "react-router-dom";
 
+import styles from './Nav.module.css';
+
 const Nav = () => {
-    <nav>
-        <NavLink to="/">
-            Home
-        </NavLink>
+    return (
+        <nav>
+            <NavLink to="/" className={({ isActive }) => {
+                return isActive
+                    ? [styles.link, styles.active].join(' ')
+                    : styles.link;
+            }}>
+                Home
+            </NavLink>
         
-        <NavLink to="movies">
-            Movies
-        </NavLink>
-    </nav>
+            <NavLink to="movies" className={({ isActive }) => {
+                return isActive
+                    ? [styles.link, styles.active].join(' ')
+                    : styles.link;
+            }}>
+                Movies
+            </NavLink>
+        </nav>
+    )
 };
 
 export default Nav;
